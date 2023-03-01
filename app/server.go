dbd0937ad1efd469dc94a3acb4fcdac06f5b41f8
mx <max@maxvl.com>
2023-02-28 22:42:07 -0500

    Handle multiple requests

--- app/server.go
+++ app/server.go
@@ -7,15 +7,22 @@ import (
 
 func main() {
 	listener, _ := net.Listen("tcp", ":6379")
-
-	conn, _ := listener.Accept()
-
-	defer conn.Close()
-
-	response := []byte("+PONG\r\n")
-	_, err := conn.Write(response)
-	if err != nil {
-		fmt.Println("Error occurred in sending reply: %s", err)
-		return
+	defer listener.Close()
+	for {
+		conn, _ := listener.Accept()
+		go handleConnection(conn)
 	}
+}
+
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+  for {
+    response := []byte("+PONG\r\n")
+    _, err := conn.Write(response)
+    if err != nil {
+      fmt.Println("Error occurred in sending reply: %s", err)
+      return
+    }
+  }
 }
